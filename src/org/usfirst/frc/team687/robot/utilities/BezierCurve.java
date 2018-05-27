21e9d5e7ce7822eb172976c33417696b44147bad
Dylan Barva <d.barva5402@gmail.com>
2018-05-27 11:33:52 -0700

    removed craig reynolds stuff

--- src/org/usfirst/frc/team687/robot/utilities/BezierCurve.java
+++ src/org/usfirst/frc/team687/robot/utilities/BezierCurve.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team687.robot.utilities;
 
 import java.util.ArrayList;
+import org.usfirst.frc.team687.robot.utilities.NerdyMath;
 
 public class BezierCurve {
-	
 	private double m_x0;
 	private double m_y0;
 	private double m_x1;
@@ -22,20 +22,13 @@ public class BezierCurve {
 	private	ArrayList<Double> m_xList = new ArrayList<Double>();
 	private ArrayList<Double> m_yList = new ArrayList<Double>();
 	private ArrayList<Double> m_angleList = new ArrayList<Double>();
-	private ArrayList<Double> m_slopeList = new ArrayList<Double>();
-	private ArrayList<Double> m_yInterceptList = new ArrayList<Double>();
-	private ArrayList<Double> m_perpendicularSlopeList = new ArrayList<Double>();
-	private ArrayList<Double> m_hypotenuseList = new ArrayList<Double>();
-	private ArrayList<Double> m_deltaXList = new ArrayList<Double>();
 	private double m_deltaX;
 	private double m_deltaY;
 	private double m_prevX;
 	private double m_prevY;
 	private double m_distance;
 	private double m_hypotenuse;
-	private double m_slope;
-	private double m_yIntercept;
-	private double m_perpendicularSlope;
+
 	
 	/*
 	 * 
@@ -50,6 +43,7 @@ public class BezierCurve {
 		m_x3 = x3;
 		m_y3 = y3;
 		m_step = step;
+		NerdyMath.addSensitivity(1, 1);
 	}
 	
 	public void generateCurve() {
@@ -66,31 +60,12 @@ public class BezierCurve {
 			m_deltaY = m_y - m_prevY;			 
             m_angle = Math.atan2(m_deltaX, m_deltaY);
             m_angle = Math.toDegrees(m_angle);
-            m_hypotenuse = NerdyMath.distanceFormula(m_x, m_y, m_prevX, m_prevY);
-            if (m_deltaX != 0) {
-            	m_slope = m_deltaY/m_deltaX;
-            }
-            else {
-            	m_slope = 0;
-            }
-            m_yIntercept = m_y - m_slope * m_x;
-            if (m_slope != 0) {
-            	m_perpendicularSlope = -Math.pow(m_slope, -1);
-            	
-            }
-            else {
-            	m_perpendicularSlope = 0;
-            }
+            m_hypotenuse = NerdyMath.distanceFormula(m_x, m_y, m_prevX, m_prevY);      
             m_distance += m_hypotenuse;
             m_tList.add(m_t);
             m_xList.add(m_x);
             m_yList.add(m_y);
             m_angleList.add(m_angle);
-            m_slopeList.add(m_slope);
-            m_yInterceptList.add(m_yIntercept);
-            m_perpendicularSlopeList.add(m_perpendicularSlope);
-            m_hypotenuseList.add(m_hypotenuse);
-            m_deltaXList.add(m_deltaX);
             m_prevX = m_x;
             m_prevY = m_y;
             m_a += 1;
@@ -120,26 +95,6 @@ public class BezierCurve {
 	
 	public double getAngle(int t) {
 		return m_angleList.get(t);
-	}
-	
-	public double getSlope(int t) {
-		return m_slopeList.get(t);
-	}
-	
-	public double getYIntercept(int t) {
-		return m_yInterceptList.get(t);
-	}
-	
-	public double getPerpendicularSlope(int t) {
-		return m_perpendicularSlopeList.get(t);
-	}
-	
-	public double getHypotenuse(int t) {
-		return m_hypotenuseList.get(t);
-	}
-	
-	public double getDeltaX(int t) {
-		return m_deltaXList.get(t);
 	}
 	
 	public double getCurveLength() {
